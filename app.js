7348337154b495655ddf813409d88149f1d53d5c
unknown <kaddour.khaled.dev@gmail.com>
2023-12-03 10:39:00 +0100

    create a basic server application

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("hello, world");
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+dotenv.config();
+
+const { PORT_NUM, DEV_MODE } = process.env;
+
+const app = express();
+app.use(express.json());
+
+// use morgan only in the developpement mode
+if (DEV_MODE === "DEV") {
+  app.use(morgan("dev"));
+}
+
+app.get("/", (req, res) => {
+  res.send({ result: "hello, world" });
+});
+
+app.listen(PORT_NUM, () => {
+  console.log(`server running in http://localhost:${PORT_NUM}`);
+});
