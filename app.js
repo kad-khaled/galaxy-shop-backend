20cd9d5ed0ed4ebf04b0aaf897d33abf18e0d241
unknown <kaddour.khaled.dev@gmail.com>
2023-12-04 14:41:22 +0100

    add validation layer to create user using the express-validator module

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const { connectToMongoDB } = require("./DB_Connection");
 const { User } = require("./UserModel");
+const { body, validationResult } = require("express-validator");
+const { Error } = require("mongoose");
 
 dotenv.config();
 
@@ -21,7 +23,37 @@ app.get("/", (req, res) => {
   res.send({ result: "hello, world" });
 });
 
-app.post("/api/v1/users", async (req, res) => {
+const postUserValidator = [
+  body("firstName").notEmpty().withMessage("The first name is required."),
+  body("lastName").notEmpty().withMessage("The last name is required."),
+  body("phoneNumber")
+    .notEmpty()
+    .withMessage("The phone number is required.")
+    .custom(async (value) => {
+      const user = await User.findOne({ phoneNumber: value });
+      if (user)
+        throw new Error("The phone number exists, and it should be unique.");
+    }),
+  body("email")
+    .optional()
+    .custom(async (value) => {
+      const user = await User.findOne({ email: value });
+      console.log(user);
+      if (user)
+        throw new Error("The email already exists, and it should be unique.");
+    }),
+  body("password").notEmpty().withMessage("The password is required."),
+];
+app.post("/api/v1/users", postUserValidator, async (req, res) => {
+  const result = validationResult(req);
+
+  if (!result.isEmpty()) {
+    res.status(400).json({
+      state: "faild",
+      stateCode: 400,
+      message: result.array().map((res) => res.msg),
+    });
+  }
   const userInfo = req.body;
 
   try {
@@ -34,12 +66,7 @@ app.post("/api/v1/users", async (req, res) => {
       data: savedUser,
     });
   } catch (err) {
-    console.log("failed to save the user");
-    res.status(400).json({
-      state: "faild",
-      stateCode: 400,
-      message: " failed to save the data in the database.",
-    });
+    console.log("failed to save the user" + err.message);
   }
 });
 
